cb336982c2274bdfb4998d2ab6d484089d758567
L30Gt <leeo12322@hotmail.com>
2023-12-12 12:16:49 -0300

    feat: add 'Usuarios' controller with methods for authentication

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -9,16 +9,15 @@ namespace CashFlowApi.Models
     public class Usuario
     {
         public int Id { get; set; }
-        public string Username { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; } = Array.Empty<byte>();
+        public byte[] PasswordSalt { get; set; } = Array.Empty<byte>();
 
         [NotMapped]
-        public string PasswordString { get; set; }
+        public string PasswordString { get; set; } = string.Empty;
         public DateTime? DataAcesso { get; set; }
 
-        public List<Lancamento> Lancamentos { get; set; }
+        public List<Lancamento>? Lancamentos { get; set; }
     }
-
 }
 
