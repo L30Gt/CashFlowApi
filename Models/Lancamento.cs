3819dd081a9f7ccfecbf3c3d7a20fc90c7e8a7d9
L30Gt <leeo12322@hotmail.com>
2023-12-12 02:19:22 -0300

    feat: add Models

---
+++ Models/Lancamento.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using CashFlowApi.Models.Enuns;
+
+namespace CashFlowApi.Models
+{
+    public class Lancamento
+    {
+        public int Id { get; set; }
+        public TipoLancamentoEnum TipoLancamento { get; set; }
+        public string Descricao { get; set; } = string.Empty;
+        
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Valor { get; set; }
+        public DateTime DataLancamento { get; set; }
+    }
+
+}
